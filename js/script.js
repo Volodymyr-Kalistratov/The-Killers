9668f0fce69c0519c44b87492f01831005a896e2
Volodymyr Kalistratov <volodymyr.kalistratov@gmail.com>
2023-04-01 21:58:03 +0200

    add gif

--- js/script.js
+++ js/script.js
@@ -5,4 +5,4 @@ let themeName = document.querySelector(".button__themeName");
 button.addEventListener("click", () => {
     body.classList.toggle("dark");
     themeName.innerText = body.classList.contains("dark") ? "jasny" : "ciemny";
-});
+}); 
